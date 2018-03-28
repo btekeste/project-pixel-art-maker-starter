f34b05632c6236f6b52898969107fb33db6597c9
Berhe Tekste <berhe.tekeste@gmail.com>
2018-03-28 07:59:09 -0400

    Function makeGrid creates the table and enables cell coloring on click

--- designs.js
+++ designs.js
@@ -3,8 +3,24 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function makeGrid(evt) {
+	const inputHeight = document.querySelector('#inputHeight').value;
+	const inputWeight = document.querySelector('#inputWeight').value;
+    const canvasGrid = document.querySelector('#pixelCanvas');
+	canvasGrid.innerHTML = '';
 
-// Your code goes here!
+	for (let i = 1; i <= inputHeight; i++) {
+		const newRow = document.createElement('tr');
+		canvasGrid.appendChild(newRow);
+		for (let j = 1; j <= inputWeight; j++) {
+			const newCell = document.createElement('td');
+			newRow.appendChild(newCell);
+			newCell.onclick = function () {
+			colorPicker = document.querySelector('#colorPicker');
+			this.style.backgroundColor = colorPicker.value;
+			};
+		}
+	}
 
+	evt.preventDefault();
 }
